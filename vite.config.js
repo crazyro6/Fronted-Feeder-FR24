dc89a42f23dd566660935a54d8a0a01c469ea079
crazyro6 <roypara@icloud.com>
2026-03-20 20:21:38 +0000

    Añadido la propiedad `base: './'` en el archivo `vite.config.js` para asegurar que los recursos se carguen correctamente cuando se despliegue la aplicación en un entorno de producción. Esto es especialmente útil si la aplicación no se sirve desde la raíz del dominio.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', 
   server: {
     proxy: {
       // Cuando hagamos fetch a '/dump1090', Vite lo mandará a tu Raspberry
